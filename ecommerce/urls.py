e799e645254f5cb1a7ec90ac8e1a30cf543f3d1f
satinamatias <satinamatias@gmail.com>
2026-04-13 22:59:38 -0300

    [WIP] Se agrego pasarela de pago

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
 ]
 
 from django.conf import settings
